c2a5e03dc25b49ead84b2eda18574030b5e6f44a
Yura <bondprcuk@gmail.com>
2021-09-17 21:21:48 +0300

    3 block ready

---
+++ src/js/script.js
@@ -0,0 +1,70 @@
+var name = "Yura";
+
+let number = "7";
+const pi = "3.14";
+
+number = 4;
+
+let leftBorderWidth = 200;
+
+// number
+// string
+// true/false
+// null
+// undefined
+// let obj = {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200,
+// }
+// Symbol
+
+// alert(1)
+// console.log(number);
+// let answer = confirm('Вам есть 18?');
+// console.log(answer);
+
+// let answer = prompt('Вам есть 18?', '');
+// console.log(answer);
+
+// console.log(4 + 'ffdd)
+
+// let isChecked = true,
+//     isClose = true;
+
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+// if (2*6 == 8*1) {
+//     console.log('Верно')
+// } else {
+//     console.log('Ошибка')
+// }
+
+// let answer = confirm('Вам есть 18?');
+// if (answer) {
+//     console.log('Проходите')
+// } else {
+//     console.log('Уходи')
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log("Неверно")
+// } else if (num > 100) {
+//     console.log('Много')
+// } else {
+//     console.log('Верно')
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+function logging(a, b) {
+    console.log(a + b)
+}
+
+logging(3, 5);
