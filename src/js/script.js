1ee1d5e4284f392aeb8bf2a81e30b41bf5b1cd2a
Yura <bondprcuk@gmail.com>
2021-09-21 23:36:55 +0300

    validation-form

--- src/js/script.js
+++ src/js/script.js
@@ -33,4 +33,52 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link')
     toggleSlide('.catalog-item__back')
+
+    // Modal
+
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+        console.log()
+    });
+
+    $(".button_mini").each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символов!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефон",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес электронной почты"
+                }
+              }
+        });
+    };
+    validateForms('#consultation_form')
+    validateForms('#consultation form')
+    validateForms('#order form')
 });
